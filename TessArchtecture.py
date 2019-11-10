eb8d75b03a20c0750b04ea082d5effccc626f4c2
lukkascost <lucas.costa@lit.ifce.edu.br>
2019-11-10 19:47:51 -0300

    Dataset files and tessArchtecture wire

--- TessArchtecture.py
+++ TessArchtecture.py
@@ -2,15 +2,22 @@ import numpy as np
 from comet_ml import Experiment
 experiment = Experiment(api_key="9F7edG4BHTWFJJetI2XctSUzM",
                         project_name="teste-lenet", workspace="lukkascost")
+experiment.set_name("TESS_07_EP_02")
 from keras import Sequential, optimizers, losses
 from keras.layers import Conv1D, AveragePooling1D, Flatten, Dense, Reshape, Activation, BatchNormalization, Dropout, \
     MaxPooling1D
-from keras.utils import to_categorical
+from keras.utils import to_categorical, np_utils
 from sklearn.model_selection import KFold, train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 data = np.loadtxt('wav_data_c1.txt', delimiter=',', dtype=object)
 data2 = np.loadtxt('wav_data_c2.txt', delimiter=',', dtype=object)
-data = np.vstack((data,data2))
+data3 = np.loadtxt('wav_data_c3.txt', delimiter=',', dtype=object)
+data4 = np.loadtxt('wav_data_c4.txt', delimiter=',', dtype=object)
+data5 = np.loadtxt('wav_data_c5.txt', delimiter=',', dtype=object)
+data6 = np.loadtxt('wav_data_c6.txt', delimiter=',', dtype=object)
+data7 = np.loadtxt('wav_data_c7.txt', delimiter=',', dtype=object)
+data = np.vstack((data,data2, data3, data4, data5, data6, data7))
 
 atts = data[:, :-1]
 labels = data[:, -1]
@@ -19,13 +26,24 @@ labels[labels == '1_Angry'] = 0
 labels[labels == '2_Angry'] = 0
 labels[labels == '1_disgust'] = 1
 labels[labels == '2_disgust'] = 1
+labels[labels == '1_fear'] = 2
+labels[labels == '2_fear'] = 2
+labels[labels == '1_happy'] = 3
+labels[labels == '2_happy'] = 3
+labels[labels == '1_surprise'] = 4
+labels[labels == '2_surprise'] = 4
+labels[labels == '1_sad'] = 5
+labels[labels == '2_sad'] = 5
+labels[labels == '1_Neutral'] = 6
+labels[labels == '2_Neutral'] = 6
 
 X_train, X_test, y_train, y_test = train_test_split(atts, labels, test_size=0.20, random_state=42)
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
-n_timesteps, n_features, n_outputs = atts.shape[1], 1, 2
-EPOCHS = 10
+print('AFTER: ', y_test)
+n_timesteps, n_features, n_outputs = atts.shape[1], 1, 7
+EPOCHS = 50
 BATCH_SIZE = 8
 
 model = Sequential()
@@ -53,7 +71,7 @@ model.add(Activation('relu'))
 model.add(Conv1D(64, 8, padding='same'))
 model.add(Activation('relu'))
 model.add(Flatten())
-model.add(Dense(2)) # Target class number
+model.add(Dense(n_outputs)) # Target class number
 model.add(Activation('softmax'))
 model.summary()
 
@@ -62,7 +80,9 @@ model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adam(), metr
 model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=True)
 _, accuracy = model.evaluate(X_test, y_test, batch_size=BATCH_SIZE, verbose=0)
 print(accuracy)
-model.save('model.txt')
+model.save('model.h5')
+experiment.log_asset("model.h5")
 model.save_weights('model.weights')
+experiment.log_asset("model.weights")
 experiment.log_other('model', model)
 experiment.end()
--- gen_dataset.py
+++ gen_dataset.py
@@ -2,30 +2,30 @@ from comet_ml import Experiment
 import scipy.io.wavfile as wav
 import numpy as np
 
-Classes = ['Angry', 'disgust']
-for c in range(1,3):
+Classes = ['Angry', 'disgust', 'fear', 'happy', 'surprise', 'sad', 'Neutral']
+for c in range(1, 8):
     data = []
     labels = []
-    for i in range (1,3):
-        for j in range(1,201):
-            if (j== 48 and i ==2 and c == 1): continue
-            data.append(wav.read('../Dataset_tess/C{2}/C{2}_{0}_{1}.wav'.format(i,j,c))[1])
-            labels.append('{}_{}'.format(i,Classes[c-1]))
+    for i in range(1, 3):
+        for j in range(1, 201):
+            if (j == 48 and i == 2 and c == 1): continue
+            if (j == 112 and i == 2 and c == 3): continue
+            data.append(wav.read('../Dataset_tess/C{2}/C{2}_{0}_{1}.wav'.format(i, j, c))[1])
+            labels.append('{}_{}'.format(i, Classes[c - 1]))
 
     x = [k.shape[0] for k in data]
 
-
     # data_matrix = np.zeros((len(x), int(np.mean(x)+np.std(x)) +1), dtype=object)
-    data_matrix = np.zeros((len(x), 52233), dtype=object)
-    print(data_matrix)
+    data_matrix = np.zeros((len(x), 47166), dtype=object)
+    # print(data_matrix)
 
-    for i,k in enumerate(data):
-        print(i,len(k))
+    for i, k in enumerate(data):
+        # print(i, len(k))
         size = len(k)
-        if size > data_matrix.shape[1]-1 : size = data_matrix.shape[1]-1
+        if size > data_matrix.shape[1] - 1: size = data_matrix.shape[1] - 1
         data_matrix[i][:size] = k[:size]
-    for i,k in enumerate(labels):
+    for i, k in enumerate(labels):
         data_matrix[i][-1] = k
     print(data_matrix.shape)
 
-    np.savetxt('Dataset/wav_data_c{}.txt'.format(c),data_matrix,delimiter=',', fmt="%s")
+    np.savetxt('Dataset/wav_data_c{}.txt'.format(c), data_matrix, delimiter=',', fmt="%s")
